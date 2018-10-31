7475c8baf962c57d152832a0a131368ee6be06f7
koyo72-4 <32035846+koyo72-4@users.noreply.github.com>
2018-10-30 22:43:30 -0400

    initial commit

---
+++ anagrams.js
@@ -0,0 +1,23 @@
+function makeAnagram(a, b) {
+    const aArray = a.split('');
+    const bArray = b.split('');
+    const aL = a.length;
+    const bL = b.length;
+    let anagramLetters = 0;
+
+    for (let i = 0; i < aArray.length; i++) {
+        for (let j = 0; j < bArray.length; j++) {
+            if (bArray.length === 0) {
+                return;
+            }
+            if (aArray[i] === bArray[j]) {
+                bArray.splice(j, 1);
+                anagramLetters += 2;
+                j = bArray.length;
+            }
+        }
+    }
+
+    const totalLetters = aL + bL;
+    return totalLetters - anagramLetters;
+}
